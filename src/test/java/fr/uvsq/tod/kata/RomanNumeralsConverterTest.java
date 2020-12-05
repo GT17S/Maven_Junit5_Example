37078cd54985dc45536aeba3b009a611ad986835
GT17S <guenanetoufik0615@gmail.com>
2020-12-05 20:45:18 +0100

    Modification de pom.xml en ajoutant les depandances necessaires pour executer les tests en Junit 5, et de plus importation de Junit5 dans la calsse test et son execution par la suite qui a marché

--- src/test/java/fr/uvsq/tod/kata/RomanNumeralsConverterTest.java
+++ src/test/java/fr/uvsq/tod/kata/RomanNumeralsConverterTest.java
@@ -1,12 +1,12 @@
 package fr.uvsq.tod.kata;
 
-import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
 public class RomanNumeralsConverterTest {
   @Test
   public void shouldConvertTheDigitI() {
-    assertEquals(1, RomanNumeralsConverter.toDecimal("I"));
-  }
+    assertEquals(1, RomanNumeralsConverter.toDecimal("I"),"1 is written in roman numers like this:I");
+   }
 }
