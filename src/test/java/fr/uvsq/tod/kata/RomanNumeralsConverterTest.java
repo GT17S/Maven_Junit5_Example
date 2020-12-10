8c00cbf9eb8c925bdb450357afc4c2d0f779975b
GT17S <guenanetoufik0615@gmail.com>
2020-12-10 16:52:34 +0100

    Realisation  de reduction de tests de 9 tests a 2 codes de tests qui realisent tous les tests demandé

--- src/test/java/fr/uvsq/tod/kata/RomanNumeralsConverterTest.java
+++ src/test/java/fr/uvsq/tod/kata/RomanNumeralsConverterTest.java
@@ -2,68 +2,27 @@ package fr.uvsq.tod.kata;
 
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class RomanNumeralsConverterTest {
 
-    @Test
-    public void shouldConvertTheDigitI() {
-        assertEquals(1, RomanNumeralsConverter.toDecimal("I"),"1 is written in roman numers like this:I");
+    @ParameterizedTest
+    @CsvSource({"I,1", "V,5", "X,10", "L,50", "C,100", "D,500", "M,1000"})
+    void conversion(String RomanNumber, int intValue) {
+        assertEquals(intValue, RomanNumeralsConverter.toDecimal(RomanNumber));
     }
 
-    @Test
-    public void shouldConvertTheDigitV() {
-        assertEquals(5, RomanNumeralsConverter.toDecimal("V"),"5 is written in roman numers like this:V");
-    }
-
-    @Test
-    public void shouldConvertTheDigitX() {
-        assertEquals(10, RomanNumeralsConverter.toDecimal("X"),"10 is written in roman numers like this:X");
-    }
-
-    @Test
-    public void shouldConvertTheDigitL() {
-        assertEquals(50, RomanNumeralsConverter.toDecimal("L"),"50 is written in roman numers like this:L");
-    }
-
-    @Test
-    public void shouldConvertTheDigitC() {
-        assertEquals(100, RomanNumeralsConverter.toDecimal("C"),"100 is written in roman numers like this:C");
-    }
-
-    @Test
-    public void shouldConvertTheDigitD() {
-        assertEquals(500, RomanNumeralsConverter.toDecimal("D"),"500 is written in roman numers like this:D");
-    }
-
-    @Test
-    public void shouldConvertTheDigitM() {
-        assertEquals(1000, RomanNumeralsConverter.toDecimal("M"),"1000 is written in roman numers like this:M");
-    }
-
-    @Test
-    void testIllegalArgumentExceptionOfEmptyBlankValuePuttenIN() {
+    @ParameterizedTest
+    @ValueSource(strings = { "","this is london" })
+    void testEmptyValueOrAnOrderedValuePuttenIN(String Value) {
 
         Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            RomanNumeralsConverter.toDecimal("");
-        });
-    }
-
-    @Test
-    void testIllegalArgumentExceptionOfNullValuePuttenIN() {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            RomanNumeralsConverter.toDecimal(null);
-        });
-    }
-
-    @Test
-    void testIllegalArgumentExceptionOfAnOrderedStringValuePuttenIN() {
-
-        Assertions.assertThrows(IllegalArgumentException.class, () -> {
-            RomanNumeralsConverter.toDecimal("THIS IS LONDON");
+            RomanNumeralsConverter.toDecimal(Value);
         });
     }
 }
